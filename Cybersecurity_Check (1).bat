6cd130e7fbb4013a88e2775ce26ea5e572aca3a8
Aarya0017 <aryah9899@gmail.com>
2024-12-04 16:29:23 +0530

    Add files via upload

---
+++ Cybersecurity_Check (1).bat
@@ -0,0 +1,8 @@
+
+@echo off
+echo Congrats, You're Hacked Now!
+echo But don't worry, you're safe. Please make sure to secure your systems!
+echo Your cybersecurity score: High
+echo.
+echo Your details have been recorded with Amit Parmar!
+pause
